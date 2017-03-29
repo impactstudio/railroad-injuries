ca8225651dd9a86eb0cdca012bdb07450a4063cc
JWizerd <jwodke@outlook.com>
2017-03-29 13:45:47 -0600

    removed pre widgets area for unused widgets areas on sidebar

--- sidebar_widgets.php
+++ sidebar_widgets.php
@@ -1,32 +1,17 @@
 <div id="sidebar-nav">
 
   <!-- first widget area -->
-	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('sidebar-widget-1')) : else : ?>
-
-	<div class="pre-widget">
-		<p><strong>Widgetized Area</strong></p>
-		<p>This panel is active and ready for you to add some widgets via the WP Admin</p>
-	</div>
+	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('sidebar-widget-1')) : ?>
 
 	<?php endif; ?>
 
   <!-- second widget area -->
-  <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('sidebar-widget-2')) : else : ?>
-
-	<div class="pre-widget">
-		<p><strong>Widgetized Area</strong></p>
-		<p>This panel is active and ready for you to add some widgets via the WP Admin</p>
-	</div>
+  <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('sidebar-widget-2')) : ?>
 
 	<?php endif; ?>
 
   <!-- third widget area -->
-  <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('sidebar-widget-3')) : else : ?>
-
-	<div class="pre-widget">
-		<p><strong>Widgetized Area</strong></p>
-		<p>This panel is active and ready for you to add some widgets via the WP Admin</p>
-	</div>
+  <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('sidebar-widget-3')) : ?>
 
 	<?php endif; ?>
 
