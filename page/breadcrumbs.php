16505861c9736449c27d5964ea35eb7ecbd7e9ac
JWizerd <jwodke@outlook.com>
2017-03-31 11:40:53 -0600

    fixed container row and col-offset issue for breadcrumbs and hero title and hero description on page layouts

--- page/breadcrumbs.php
+++ page/breadcrumbs.php
@@ -1,11 +1,7 @@
 <section class="breadcrumb-container">
   <div class="container">
-    <div class="row">
-      <div class="col-lg-12 col-lg-offset-0 col-sm-10 col-sm-offset-1">
-        <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
-          <?php if(function_exists('bcn_display')) { bcn_display(); } ?>
-        </div>
+      <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+        <?php if(function_exists('bcn_display')) { bcn_display(); } ?>
       </div>
-    </div>
   </div>
 </section>
--- page/hero.php
+++ page/hero.php
@@ -12,11 +12,9 @@
     class="hero-image">
   <div class="black-overlay z-1"></div>
   <div class="container">
-    <div class="row">
       <div class="subpage-hero-content z-2">
         <h1><?php the_title(); ?></h1>
         <p class="lead"><?php echo $hero_description; ?></p>
       </div>
-    </div>
   </div>
 </div>
