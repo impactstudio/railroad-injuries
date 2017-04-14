cb48efd8434eb29b9e5c83f11ca64b1c969d3271
Sam Ringleman <sam.rinleman@gmail.com>
2017-04-14 14:42:46 -0600

    add the lawyers section and pull the info with acf

--- home/layout.php
+++ home/layout.php
@@ -5,6 +5,7 @@
   <?php get_template_part('home/cases'); ?>
   <?php get_template_part('home/settlements'); ?>
   <?php get_template_part('home/consultation_banner'); ?>
+  <?php get_template_part('home/lawyer_section'); ?>
 </main>
 
 <?php get_footer(); ?>
