3f4e78f50e0fbfa29d6679c445a983b4c24cd1bc
JWizerd <jwodke@outlook.com>
2017-04-14 14:45:42 -0600

    footer links styled

--- footer/footer_links.php
+++ footer/footer_links.php
@@ -1,14 +1,38 @@
+<?php
+  $facebook =               get_field('facebook', 'option');
+  $gplus =                  get_field('google_plus', 'option');
+  $linkedin =               get_field('linkedin', 'option');
+?>
+
 <div class="footer-links-wrapper">
   <div class="container">
-    <ul class="footer-links pull-left">
-      <li>©2017 Copyright Rossi Vucinovich PC. All Rights Reserved.</li>
-      <li>Privacy Policy</li>
-      <li>Disclaimer</li>
-    </ul>
-    <ul class="pull-right social_links">
-      <li class="social_icon"><i class="fa fa-facebook-square denim"></i></li>
-      <li class="social_icon"><i class="fa fa-google-plus-square denim"></i></li>
-      <li class="social_icon"><i class="fa fa-linkedin-square denim"></i></li>
-    </ul>
-  </div>
+    <div class="footer-links">
+      <ul>
+        <li>©2017 Copyright Rossi Vucinovich PC. All Rights Reserved.</li>
+        <li>
+          <a href="">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="">Disclaimer</a>
+        </li>
+      </ul>
+      <ul class="social_links">
+        <li class="social_icon">
+          <a href="<?php echo $facebook; ?>">
+            <i class="fa fa-facebook-square denim"></i>
+          </a>
+        </li>
+        <li class="social_icon">
+          <a href="<?php echo $gplus; ?>">
+            <i class="fa fa-google-plus-square denim"></i>
+          </a>
+        </li>
+        <li class="social_icon">
+          <a href="<?php echo $linkedin; ?>">
+            <i class="fa fa-linkedin-square denim"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+    </div>
 </div>
