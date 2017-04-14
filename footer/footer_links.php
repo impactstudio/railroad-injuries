7c0144fcd2838e98e3b7292853649004b37e2125
JWizerd <jwodke@outlook.com>
2017-04-14 12:54:18 -0600

    custom fields directory

--- footer/footer_links.php
+++ footer/footer_links.php
@@ -1,2 +1,14 @@
-<ul class="footer-links container-fluid">
-</ul>
+<div class="footer-links-wrapper">
+  <div class="container">
+    <ul class="footer-links pull-left">
+      <li>©2017 Copyright Rossi Vucinovich PC. All Rights Reserved.</li>
+      <li>Privacy Policy</li>
+      <li>Disclaimer</li>
+    </ul>
+    <ul class="pull-right social_links">
+      <li class="social_icon"><i class="fa fa-facebook-square denim"></i></li>
+      <li class="social_icon"><i class="fa fa-google-plus-square denim"></i></li>
+      <li class="social_icon"><i class="fa fa-linkedin-square denim"></i></li>
+    </ul>
+  </div>
+</div>
