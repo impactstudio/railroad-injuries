06316e868c4619777e8dc80e70b39f0058c2588c
JWizerd <jwodke@outlook.com>
2017-04-14 13:33:18 -0600

    bg_lawyers

--- sidebar/lawyers.php
+++ sidebar/lawyers.php
@@ -1,7 +1,7 @@
 <?php
 
 $args = [
-  'post_type' => 'lawyer',
+  'post_type' => 'lawyer'
 ];
 
 $lawyers = new WP_Query($args);
@@ -9,9 +9,6 @@ $lawyers = new WP_Query($args);
 ?>
 
 <?php if ($lawyers->have_posts()) :  while($lawyers->have_posts()) : $lawyers->the_post(); ?>
-
-  <?php $role = get_field('role'); ?>
-
   <a href="<?php the_permalink(); ?>">
     <img
       class="sidebar-lawyer"
@@ -23,6 +20,5 @@ $lawyers = new WP_Query($args);
       <?php the_title(); ?>
     </a>
   </h5>
-  <p class="attorney-role"><?php echo $role; ?></p>
-
+  <p class="attorney-role">President of RVPC</p>
 <?php endwhile; endif; ?>
